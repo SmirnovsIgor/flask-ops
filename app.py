42de50ecd58ece2b79177c24e0429402460e907e
Igor Smirnov <igorsmirnov@Igors-MacBook-Air.local>
2020-06-26 22:14:33 +0300

    Add tests to pipeline

--- app.py
+++ app.py
@@ -11,18 +11,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 # !! DON'T EXPOSE THIS KEY !!
 SUPER_SECRET_SALT = "68874260280172957479"
 
+
 def encrypt_string(hash_string):
     sha_signature = \
-    	hashlib.sha256(hash_string.encode()).hexdigest()
+        hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
+
 
 def generate():
     rand_device = string.ascii_letters + string.digits
-    return encrypt_string((''.join(secrets.choice(rand_device) for i in range(8))).join(SUPER_SECRET_SALT))
+    return encrypt_string((''.join(secrets.choice(rand_device)
+                                   for i in range(8))).join(SUPER_SECRET_SALT))
+
 
 @app.route('/')
 def signature():
---
+++ tests.py
@@ -0,0 +1,23 @@
+"""
+Unit tests for flask-ops application
+"""
+import unittest
+
+from app import encrypt_string
+
+
+class TestApp(unittest.TestCase):
+    """
+    Test app.py
+    """
+    def test_encrypt_string_empty(self):
+        self.assertEqual(encrypt_string(''),
+                         'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
+
+    def test_encrypt_string_lower_block_size(self):
+        self.assertEqual(encrypt_string('qwerty'),
+                         '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5')
+
+    def test_encrypt_string_equal_block_size(self):
+        self.assertEqual(encrypt_string('a' * 64),
+                         'ffe054fe7ae0cb6dc65c3af9b61d5209f439851db43d0ba5997337df154668eb')
