c87aec6bdc017c8bf613eb54154eee457df26003
Igor Smirnov <igorsmirnov@Igors-MacBook-Air.local>
2020-06-26 13:17:43 +0300

    Fix pylint under score

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+"""
 ### ACME CORP
 ### Customer session key generator. Protects PII.
 ### / 2020
+"""
 import string
 import secrets
 import hashlib
@@ -11,17 +13,16 @@ app = Flask(__name__)
 
 
 # !! DON'T EXPOSE THIS KEY !!
-super_secret_salt = "68874260280172957479"
+SUPER_SECRET_SALT = "68874260280172957479"
 
 def encrypt_string(hash_string):
     sha_signature = \
-        hashlib.sha256(hash_string.encode()).hexdigest()
+    	hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
 
-# Generate a simple key
 def generate():
     rand_device = string.ascii_letters + string.digits
-    return encrypt_string((''.join(secrets.choice(rand_device) for i in range(8))).join(super_secret_salt))
+    return encrypt_string((''.join(secrets.choice(rand_device) for i in range(8))).join(SUPER_SECRET_SALT))
 
 @app.route('/')
 def signature():
