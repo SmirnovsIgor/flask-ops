d9bbe0049f69f521bbb161161a24e81537a27d6f
Igor Smirnov <igorsmirnov@Igors-MacBook-Air.local>
2020-06-28 15:13:31 +0300

    Fix workflow

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 export FLASK_APP=app.py
-flask run --host=0.0.0.0 --port=5000
+if [ -z "$PORT" ]
+then
+  flask run --host=0.0.0.0 --port=5000
+else
+  flask run --host=0.0.0.0 --port="$PORT"
+fi
